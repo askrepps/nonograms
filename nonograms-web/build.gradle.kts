71739a31760a77a456dc0f9ffdeeaaad6ab5109c
askrepps <askrepps@gmail.com>
2022-07-30 12:55:14 -0400

    Use default npm package versions corresponding to the latest Kotlin multiplatform plugin version

--- nonograms-web/build.gradle.kts
+++ nonograms-web/build.gradle.kts
@@ -22,9 +22,6 @@
  * SOFTWARE.
  */
 
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
-
 plugins {
     kotlin("multiplatform")
 }
@@ -34,18 +31,6 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
-}
-
-rootProject.plugins.withType(YarnPlugin::class.java) {
-    rootProject.the<NodeJsRootExtension>().apply {
-        versions.webpackDevServer.version = "4.8.1"
-        versions.karma.version = "6.3.16"
-        versions.karmaChromeLauncher.version = "3.1.0"
-        versions.karmaMocha.version = "2.0.1"
-        versions.karmaSourcemapLoader.version = "0.3.8"
-        versions.karmaWebpack.version = "5.0.0"
-        versions.mocha.version = "9.2.0"
-    }
 }
 
 kotlin {
